63954b1d08538055f7c841d4dc86ec6b0cf802af
Antshel23 <antsheldon23@icloud.com>
2024-10-03 20:38:51 +0100

    login and logout + tests

--- src/User.js
+++ src/User.js
@@ -10,11 +10,26 @@ class User {
     }
 
 login(password) {
-
+if (password == this.password) {
+    this.loggedIn = true
+    return true
+    console.log('Logged in')
+}
+else {
+    throw Error('Incorrect password')
+    return false
+}
 }
 
 logout() {
-
+    if (this.loggedIn = true) {
+this.loggedIn = false
+ console.log('Logged out')
+return true
+    }
+    else {
+        throw Error ('Already logged out')
+    }
 }
 }
 
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,3 +1,25 @@
 const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 const ScooterApp = require('../src/ScooterApp')
+
+describe('test constructor', () => {
+    test('constructor values as expected', () => {
+        const user1 = new User('Antshel23', 'pass', 22)
+        expect(user1).toEqual({username: 'Antshel23', password: 'pass', age: 22, loggedIn: false})
+    })
+})
+
+describe('login function', () => {
+    test('login possible', () => {
+        const user1 = new User('Antshel23', 'pass', 22)
+        expect(user1.login('pass')).toEqual(true)
+    })
+})
+
+describe('logout function', () => {
+    test('logout possible', () => {
+        const user1 = new User('Antshel23', 'pass', 22)
+        user1.login('pass')
+        expect(user1.logout()).toEqual(true)
+    })
+})
