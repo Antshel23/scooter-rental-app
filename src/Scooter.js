fa3e2538329dbb227af665d35f386865c4559593
Antshel23 <antsheldon23@icloud.com>
2024-10-03 16:45:16 +0100

    completed constructor test for scooter

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,12 @@
 class Scooter {
+    static nextSerial = 1
+
     constructor(station) {
         this.station = station
+        this.user = null
+        this.serial = Scooter.nextSerial++
+        this.charge = 100
+        this.isBroken = false
     }
 
 }
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,8 +1,9 @@
 const Scooter = require('../src/Scooter')
 
 describe('test constructor', () => {
-    test('station set', () => {
+    test('constructor values ', () => {
         const scooter1 = new Scooter('euston')
-        expect(scooter1.station).toBe('euston')
+        const scooter2 = new Scooter('euston')
+        expect(scooter2).toEqual({station: 'euston', user: null, serial: 2, charge: 100, isBroken: false})
     })
 })
